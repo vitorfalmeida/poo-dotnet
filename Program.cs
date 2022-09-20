4701cb1f7c6d4cbe227d6fc68c7a0e95d3280818
Vitor Almeida <vitororama15@gmail.com>
2022-09-20 17:36:56 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Treino.Models;
+
+
+Pessoa pessoa01 = new Pessoa
+{
+    Nome = "Vitor",
+    Idade = 29
+};
+
+pessoa01.Apresentar();
